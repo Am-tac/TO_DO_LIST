22fa5748fc77d53dd5dac0ccc373d4120e3068ab
Amir hosein nosrati <168615493+Am-tac@users.noreply.github.com>
2024-07-27 15:03:21 +0330

    Add files via upload

--- edit/edit.php
+++ edit/edit.php
@@ -2,13 +2,13 @@
 
 include_once "../conected/conect.php";
 
-if (isset($_GET["id"])&& isset($_GET['name'])) {
+if (isset($_GET["id"]) && isset($_GET['name'])) {
 
     $list_id = $_GET["id"];
 
     $qury = "SELECT * FROM work WHERE id = :id";
     $edit = $save->prepare($qury);
-    $edit->execute(['id' => $list_id]) ;
+    $edit->execute(['id' => $list_id]);
     $edit = $edit->fetch();
 }
 if (isset($_POST['edited'])) {
@@ -17,7 +17,7 @@ if (isset($_POST['edited'])) {
         $edited = $_POST["name"];
         $set = "UPDATE Work SET name =:name WHERE id=:id";
         $edit_update = $save->prepare($set);
-        $edit_update->execute([ 'id' => $list_id , 'name' => $edited ]);
+        $edit_update->execute(['id' => $list_id, 'name' => $edited]);
 
         header("Location:../index.php");
         // exit();
@@ -35,36 +35,36 @@ if (isset($_POST['edited'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/bootstrap.rtl.min.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="./style.css">
     <title>لیست کار های روزانه</title>
 </head>
 
 <body>
 
-    <div>
-        <div class="row">
-            <div>
-                <h3>ویرایش کار</h3>
-            </div>
-            <div>
-                <?php
-                if (isset($_GET['EROE_MASAIGE'])) {
-                ?>
-                    <div class="alert alert-danger" role="alert">
-                        <?php echo $_GET['EROE_MASAIGE'] ?>
-                    </div>
-                <?php
-                }
-                ?>
-            </div>
+    
 
+    <div class="row">
+        <div class="col-4">
+
+        </div>
+        <div class="col-4 " id="box">
+            <h3 class="mb-4">ویرایش کار </h3>
             <form method="post">
-                <div class="form-group">
-                    <label for="editor">عنوان کار: </label>
-                    <input type="text" class="form-control" value="<?php echo $edit['name'] ?>" name="name" id="editor">
+                <div class="input-group">
+                    <p for="editor" class="me-4">عنوان کار:</p>
+                    <input type="text" class="form-control ms-3" value="<?php echo $edit['name'] ?>" name="name" id="editor">
+                    <button type="submit" name="edited" class="btn btn-outline-success">ویرایش</button>
                 </div>
-                <button type="submit" name="edited" class="btn btn-outline-primary">ویرایش</button>
             </form>
+            <?php
+            if (isset($_GET['EROE_MASAIGE'])) {
+            ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $_GET['EROE_MASAIGE'] ?>
+                </div>
+            <?php
+            }
+            ?>
         </div>
     </div>
 
